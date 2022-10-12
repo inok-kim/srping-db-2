e7dd44e27351d1b632681276b27a9ede9f141e1a
iokim <iokim1220@gmail.com>
2022-10-12 12:17:08 +0900

    feat: MyBatis

---
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,24 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    // 파라미터가 2개 이상인 경우 @Param을 적어줘야함
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
+
+    List<Item> findAll(ItemSearchCond itemSearch);
+
+    Optional<Item> findById(Long id);
+
+}
